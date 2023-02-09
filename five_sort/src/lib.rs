6b6d7b60d3f9429413e3ec6027ad51b5ec009a1e
Anup Jadhav <anup.jadhav@gmail.com>
2023-02-09 14:23:20 +0000

    five sort algorithm ✅

--- five_sort/src/lib.rs
+++ five_sort/src/lib.rs
@@ -1,5 +1,3 @@
-use std::borrow::Cow;
-
 /// Five_sort takes in an array of numbers as an
 /// argument. The function should rearrange
 /// elements of the array such that all 5s
@@ -10,12 +8,34 @@ use std::borrow::Cow;
 /// Elements that are not 5 can appear in
 /// any order in the output, as long as
 /// all 5s are at the end of the array.
-pub fn five_sort(nums: &[u32]) -> Cow<[u32]> {
-    let mut slice =
-        nums.iter().filter(|x| **x != 5).cloned().collect::<Vec<u32>>();
+pub fn five_sort(nums: &mut [u32]) -> &[u32] {
+    // i is the pointer that traverses
+    // the slice from the left
+    let mut i: usize = 0;
 
-    slice.extend(nums.iter().filter(|x| **x == 5));
-    slice.into()
+    // and j traverses the array/slice
+    // from the right
+    let mut j: usize = nums.len() - 1;
+
+    // loop until i & j meet in the middle
+    while i <= j {
+        // if `j` is pointing to 5
+        // then move the pointer left
+        if nums.get(j) == Some(&5) {
+            j -= 1;
+        } else if nums.get(i) == Some(&5) {
+            // if `i` is pointing to 5
+            // and `j` is NOT pointing to 5
+            // then swap i and j values
+            nums.swap(i, j);
+            i += 1;
+        } else {
+            // otherwise just increment
+            // i
+            i += 1;
+        }
+    }
+    nums
 }
 
 #[cfg(test)]
@@ -25,24 +45,24 @@ mod tests {
     #[test]
     fn test_five_sort() {
         assert_eq!(
-            five_sort(&[12, 5, 1, 5, 12, 7]).to_vec(),
-            &[12, 1, 12, 7, 5, 5]
+            five_sort(&mut [12, 5, 1, 5, 12, 7]),
+            &[12, 7, 1, 12, 5, 5]
         );
         assert_eq!(
-            five_sort(&[5, 2, 5, 6, 5, 1, 10, 2, 5, 5]).to_vec(),
-            &[2, 6, 1, 10, 2, 5, 5, 5, 5, 5]
+            five_sort(&mut [5, 2, 5, 6, 5, 1, 10, 2, 5, 5]),
+            &[2, 2, 10, 6, 1, 5, 5, 5, 5, 5]
         );
         assert_eq!(
-            five_sort(&[5, 5, 5, 1, 1, 1, 4]).to_vec(),
-            &[1, 1, 1, 4, 5, 5, 5]
+            five_sort(&mut [5, 5, 5, 1, 1, 1, 4]),
+            &[4, 1, 1, 1, 5, 5, 5]
         );
         assert_eq!(
-            five_sort(&[5, 5, 6, 5, 5, 5, 5]).to_vec(),
+            five_sort(&mut [5, 5, 6, 5, 5, 5, 5]),
             &[6, 5, 5, 5, 5, 5, 5]
         );
         assert_eq!(
-            five_sort(&[5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]).to_vec(),
-            &[1, 2, 3, 2, 1, 4, 5, 5, 5, 5, 5, 5, 5, 5]
+            five_sort(&mut [5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]),
+            &[4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
         );
     }
 }
