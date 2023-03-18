1c309ff4bbae9ecef2ae99e75abecc001fb627de
Anup Jadhav <anup.jadhav@gmail.com>
2023-03-18 08:29:21 +0000

    Finished the graph island count 🏝️  algorithm

--- graph_data_structure/island_count/src/lib.rs
+++ graph_data_structure/island_count/src/lib.rs
@@ -1,28 +1,12 @@
 use std::collections::HashSet;
 
-// pub fn island_count<Matrix: AsRef<[Row]>, Row: AsRef<[char]>>(grid: Matrix) -> isize {
-//     let mut visited: HashSet<char> = HashSet::new();
-//     let mut count = 0;
-//
-//     for row in grid.as_ref() {
-//         for col in row.as_ref() {
-//             print!("{} ", col);
-//         }
-//         println!("");
-//     }
-//
-//     count
-// }
-
-#[allow(dead_code)]
-pub fn island_count<const N: usize>(grid: &[[char; N]]) -> isize {
+pub fn island_count<const N: usize>(grid: &[[char; N]]) -> usize {
     let mut visited: HashSet<String> = HashSet::new();
     let mut count = 0;
 
     for (row_num, row) in grid.iter().enumerate() {
         for (col_num, _) in row.iter().enumerate() {
             if explore(grid, row_num as isize, col_num as isize, &mut visited)
-                == true
             {
                 count += 1;
             }
@@ -32,12 +16,11 @@ pub fn island_count<const N: usize>(grid: &[[char; N]]) -> isize {
     count
 }
 
-#[allow(dead_code)]
 pub fn explore<const N: usize>(
     grid: &[[char; N]],
     row_num: isize,
     col_num: isize,
-    mut visited: &mut HashSet<String>,
+    visited: &mut HashSet<String>,
 ) -> bool {
     let row_in_bounds = { row_num >= 0 && row_num < grid.len() as isize };
     let col_in_bounds = { col_num >= 0 && col_num < grid[0].len() as isize };
@@ -56,10 +39,10 @@ pub fn explore<const N: usize>(
     }
     visited.insert(pos);
 
-    explore(&grid, row_num - 1, col_num, &mut visited);
-    explore(&grid, row_num + 1, col_num, &mut visited);
-    explore(&grid, row_num, col_num - 1, &mut visited);
-    explore(&grid, row_num, col_num + 1, &mut visited);
+    explore(grid, row_num - 1, col_num, visited);
+    explore(grid, row_num + 1, col_num, visited);
+    explore(grid, row_num, col_num - 1, visited);
+    explore(grid, row_num, col_num + 1, visited);
 
     true
 }
@@ -80,5 +63,30 @@ mod tests {
         ];
         let result = island_count::<5>(&grid);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn island_count_test_01() {
+        let grid = [
+            ['L', 'W', 'W', 'L', 'W'],
+            ['L', 'W', 'W', 'L', 'L'],
+            ['W', 'L', 'W', 'L', 'W'],
+            ['W', 'W', 'W', 'W', 'W'],
+            ['W', 'W', 'L', 'L', 'L'],
+        ];
+        let result = island_count::<5>(&grid);
+        assert_eq!(result, 4);
+    }
+    #[test]
+    fn island_count_test_02() {
+        let grid = [['L', 'L', 'L'], ['L', 'L', 'L'], ['L', 'L', 'L']];
+        let result = island_count::<3>(&grid);
+        assert_eq!(result, 1);
+    }
+    #[test]
+    fn island_count_test_03() {
+        let grid = [['W', 'W'], ['W', 'W'], ['W', 'W']];
+        let result = island_count::<2>(&grid);
+        assert_eq!(result, 0);
     }
 }
