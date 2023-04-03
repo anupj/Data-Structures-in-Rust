1c5f7f6648df65e0645e5d9aedd1045b3d4a35ab
Anup Jadhav <anup.jadhav@gmail.com>
2023-04-03 05:49:44 +0100

    Calculate max sum possible algorithm

---
+++ dynamic_programming/max_path_sum/src/lib.rs
@@ -0,0 +1,120 @@
+use std::collections::HashMap;
+
+/// Takes in a grid as an argument. In the grid,
+/// 'X' represents walls and 'O' represents open
+/// spaces. You may only move down or to the right
+/// and cannot pass through walls. The function
+/// should return the number of ways possible to
+/// travel from the top-left corner of the grid
+/// to the bottom-right corner.
+///
+pub fn max_path_sum<const N: usize>(grid: &[[usize; N]]) -> usize {
+    max_path_with_cache(grid, 0, 0, &mut HashMap::new())
+}
+
+pub fn max_path_with_cache<const N: usize>(
+    grid: &[[usize; N]],
+    row_num: usize,
+    col_num: usize,
+    cache: &mut HashMap<String, usize>,
+) -> usize {
+    let pos = format!("{},{}", row_num, col_num);
+
+    // If the calculated value is in cache
+    // then return it
+    if let Some(&value) = cache.get(&pos) {
+        return value;
+    }
+
+    if row_num == grid.len() || col_num == grid[0].len() {
+        return usize::MIN;
+    }
+
+    if row_num == grid.len() - 1 && col_num == grid[0].len() - 1 {
+        return grid[row_num][col_num];
+    }
+
+    let down_path = max_path_with_cache(grid, row_num + 1, col_num, cache);
+    let right_path = max_path_with_cache(grid, row_num, col_num + 1, cache);
+
+    let result = grid[row_num][col_num] + down_path.max(right_path);
+    cache.insert(pos, result);
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count_paths_00() {
+        let grid = [[1, 3, 12], [5, 1, 1], [3, 6, 1]];
+        let result = max_path_sum::<3>(&grid);
+        assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn count_paths_01() {
+        let grid = [[1, 2, 8, 1], [3, 1, 12, 10], [4, 0, 6, 3]];
+        let result = max_path_sum::<4>(&grid);
+        assert_eq!(result, 36);
+    }
+
+    #[test]
+    fn count_paths_02() {
+        let grid = [[1, 2, 8, 1], [3, 10, 12, 10], [4, 0, 6, 3]];
+        let result = max_path_sum::<4>(&grid);
+        assert_eq!(result, 39);
+    }
+
+    #[test]
+    fn count_paths_03() {
+        let grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ];
+        let result = max_path_sum::<13>(&grid);
+        assert_eq!(result, 27);
+    }
+
+    #[test]
+    fn count_paths_04() {
+        let grid = [
+            [1, 1, 3, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 2, 1, 1, 6, 1, 1, 5, 1, 1, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 5, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [2, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1],
+            [2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 42, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ];
+        let result = max_path_sum::<13>(&grid);
+        assert_eq!(result, 82);
+    }
+}
