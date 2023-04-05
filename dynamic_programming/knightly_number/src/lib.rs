a74895b6354dea26d2ba2816860540082efd08a8
Anup Jadhav <anup.jadhav@gmail.com>
2023-04-05 15:19:44 +0100

    Knightly number algorithm completed

--- dynamic_programming/knightly_number/src/lib.rs
+++ dynamic_programming/knightly_number/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// A knight is on a chess board. Can you figure out
 /// the total number of ways the knight can move to a
 /// target position in exactly m moves? On a single
@@ -24,8 +26,56 @@
 /// of the board. You can assume that rows and columns
 /// are 0-indexed. This means that if n = 8, there are
 /// 8 rows and 8 columns numbered 0 to 7.
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn knightly_number(n: isize, m: isize, kr: isize, kc: isize, pr: isize, pc: isize) -> usize {
+    knightly_number_recursive(n, m, kr, kc, pr, pc, &mut HashMap::new()) as usize
+}
+
+pub fn knightly_number_recursive(
+    n: isize,
+    m: isize,
+    kr: isize,
+    kc: isize,
+    pr: isize,
+    pc: isize,
+    memo: &mut HashMap<String, isize>,
+) -> isize {
+    let key = format!("{},{},{}", m, kr, kc);
+    if let Some(&value) = memo.get(&key) {
+        return value;
+    }
+
+    if kr < 0 || kr >= n || kc < 0 || kc >= n {
+        return 0;
+    }
+
+    if m == 0 {
+        if kr == pr && kc == pc {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    let neighbours = [
+        [kr + 2, kc + 1],
+        [kr - 2, kc + 1],
+        [kr + 2, kc - 1],
+        [kr - 2, kc - 1],
+        [kr + 1, kc + 2],
+        [kr - 1, kc + 2],
+        [kr + 1, kc - 2],
+        [kr - 1, kc - 2],
+    ];
+
+    let mut count = 0;
+    for neighbour in neighbours {
+        let neighbour_row = neighbour[0];
+        let neighbour_col = neighbour[1];
+        count += knightly_number_recursive(n, m - 1, neighbour_row, neighbour_col, pr, pc, memo);
+    }
+    memo.insert(key, count);
+
+    count
 }
 
 #[cfg(test)]
@@ -33,8 +83,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn knightly_number_00() {
+        let result = knightly_number(8, 2, 4, 4, 5, 5);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn knightly_number_01() {
+        let result = knightly_number(8, 2, 7, 1, 7, 1);
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn knightly_number_02() {
+        let result = knightly_number(8, 2, 5, 4, 5, 4);
+        assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn knightly_number_03() {
+        let result = knightly_number(8, 3, 5, 2, 4, 4);
+        assert_eq!(result, 21);
+    }
+
+    #[test]
+    fn knightly_number_04() {
+        let result = knightly_number(20, 6, 18, 7, 10, 15);
+        assert_eq!(result, 60);
+    }
+
+    #[test]
+    fn knightly_number_05() {
+        let result = knightly_number(20, 12, 8, 3, 9, 14);
+        assert_eq!(result, 98410127);
+    }
+
+    #[test]
+    fn knightly_number_06() {
+        let result = knightly_number(8, 2, 0, 0, 1, 1);
+        assert_eq!(result, 0);
     }
 }
