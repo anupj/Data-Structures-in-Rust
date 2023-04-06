b52a73b82ec818bad15ec7be14962557511503ab
Anup Jadhav <anup.jadhav@gmail.com>
2023-04-06 20:28:04 +0100

    Added some sensible comments

--- exhaustive_recursion/subsets/src/lib.rs
+++ exhaustive_recursion/subsets/src/lib.rs
@@ -1,3 +1,12 @@
+/// Takes in an array as an argument. The function
+/// should return a 2D array where each subarray
+/// represents one of the possible subsets of the
+/// array.
+///
+/// The elements within the subsets and the
+/// subsets themselves may be returned in any order.
+///
+/// You may assume that the input array contains unique elements.
 pub fn subsets(elements: &[char]) -> Vec<Vec<char>> {
     if elements.is_empty() {
         return vec![vec![]];
